5e0046ea916cd2aafc840ebe124e82bfc7b0e053
enviedx <shelvin27@hotmail.com>
2023-04-10 21:26:41 +1000

    updates

--- firefox-build.ps1
+++ firefox-build.ps1
@@ -4,30 +4,27 @@ $uBlockDL = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/a
 $uBlockAppID = "uBlock0@raymondhill.net"
 $req1 = "https://hg.mozilla.org/mozilla-central/raw-file/tip/other-licenses/7zstub/firefox/7zSD.Win32.sfx"
 $req2 = "https://hg.mozilla.org/mozilla-central/raw-file/tip/browser/installer/windows/app.tag"
-$workdir = "F:\Installers\FirefoxBuild"
 
 #Use Temporary Dir
-$tempDirectory = [System.IO.Path]::GetTempPath()
-Set-Location $tempDirectory
+$baseFolder = [System.IO.Path]::GetTempPath()
+$subfolderName = "firefox-build"
 
-#Create Build Directory
-try {
-    if(Test-Path $workdir) {
-        Remove-Item $workdir -Recurse -Force
-    }
-    New-Item -ItemType Directory -Path $workdir
-    Write-Host "Directory '$workdir' deleted and recreated successfully"
-} catch {
-    Write-Host "An error occurred while deleting or creating the directory. Error details: $_"
+$subfolderPath = [System.IO.Path]::Combine($baseFolder, $subfolderName)
+
+if (Test-Path $subfolderPath) {
+    Remove-Item $subfolderPath -Recurse -Force
 }
 
-#Set Working Directory, otherwise last step fails
-Set-Location $workdir
+New-Item -ItemType Directory -Path $subfolderPath -Force | Out-Null
+
+$tempDirectory = $subfolderPath
+Set-Location $tempDirectory
+
 
 #Download Firefox
 Write-Host "Downloading Firefox..." -ForegroundColor Magenta
 try {
-    Start-BitsTransfer -Source $FirefoxDL -Destination $workdir\"Firefox.exe" -ErrorAction Stop
+    Start-BitsTransfer -Source $FirefoxDL -Destination $tempDirectory\"Firefox.exe" -ErrorAction Stop
 }
 catch {
     Write-Host "An error occurred while downloading Firefox. Error details: $_" -ForegroundColor Red
@@ -35,10 +32,10 @@ catch {
 
 #7z Firefox
 Write-Host "Extracting Firefox..." -ForegroundColor Magenta
-7z x "$workdir\Firefox.exe"
+7z x "$tempDirectory\Firefox.exe"
 
 #Create extensions directory
-$extensionsdir = New-Item -Path $workdir\core\distribution\extensions -ItemType Directory -Force
+$extensionsdir = New-Item -Path $tempDirectory\core\distribution\extensions -ItemType Directory -Force
 
 #Download ublock extension
 Write-Host "Downloading uBlock to Extensions Directory"
@@ -50,12 +47,9 @@ Write-Host "Repacking..." -ForegroundColor Magenta
 
 #Download requirements
 Write-Host "Downloading Requirements"
-rclone.exe copyurl $req1 $workdir -a --progress --stats-one-line -q
-rclone.exe copyurl $req2 $workdir -a --progress --stats-one-line -q
+rclone.exe copyurl $req1 $tempDirectory -a --progress --stats-one-line -q
+rclone.exe copyurl $req2 $tempDirectory -a --progress --stats-one-line -q
 
 #Final package
 Write-Host "Create new install file"
 cmd /c "copy /B 7zSD.Win32.sfx+app.tag+app.7z our_new_installer.exe"
-
-#Copy the new install.exe to installations directory, which will let packer access it
-Copy-Item "our_new_installer.exe" "F:\Installers\Firefox.exe" -Force
